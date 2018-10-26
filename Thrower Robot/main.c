e334bbbe85f2fb81657c30693a30cb449f323e23
Ethan <43922849+ethanppl@users.noreply.github.com>
2018-10-26 11:36:00 +0800

    Create main.c

---
+++ Thrower Robot/main.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include "rcc.h"
+#include "ticks.h"
+#include "gpio.h"
+#include "uart.h"
+#include "lcd_main.h"
+#include "oled.h"
+#include "camera.h"
+#include "pwm.h"
+#include "adc.h"
+
+int main() {
+    // Initialize Everything Here
+    rcc_init();
+    ticks_init();        
+    
+    uint32_t lastticks=get_ticks();
+    while(1){
+        if(lastticks!=get_ticks()){
+            lastticks=get_ticks();
+            if (!(lastticks%250)){
+              //code here will run every 250 ms
+              
+            }
+        }
+    }        
+}
+
