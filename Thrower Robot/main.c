046aed33c6fdeef08d854afb0ca9d434b4dcde9f
Ethan <43922849+ethanppl@users.noreply.github.com>
2018-11-06 21:33:47 +0800

    Update main.c with init sonar and timer

--- Thrower Robot/main.c
+++ Thrower Robot/main.c
@@ -34,14 +34,42 @@ void UARTOnReceiveHandler(const u8 received){
 
 int main() {
     // Initialize Everything Here
+    char buffer[80] = {'\0'};
+
+    SetSysClockTo72();
+
+    // TIMER4 Äâ³÷è çà ñåêóíäó âèêëèêàº sonar_start(); ³ âñòàíîâëþº FLAG_ECHO = 1;
+    TIM_TimeBaseInitTypeDef TIMER_InitStructure;
+    NVIC_InitTypeDef NVIC_InitStructure;
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM4, ENABLE);
+    TIM_TimeBaseStructInit(&TIMER_InitStructure);
+    TIMER_InitStructure.TIM_CounterMode = TIM_CounterMode_Up;
+    TIMER_InitStructure.TIM_Prescaler = 7200;
+    TIMER_InitStructure.TIM_Period = 5000;
+    TIM_TimeBaseInit(TIM4, &TIMER_InitStructure);
+    TIM_ITConfig(TIM4, TIM_IT_Update, ENABLE);
+    TIM_Cmd(TIM4, ENABLE);
+
+    /* NVIC Configuration */
+    /* Enable the TIM4_IRQn Interrupt */
+    NVIC_InitStructure.NVIC_IRQChannel = TIM4_IRQn;
+    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
+    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
+    NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+    NVIC_Init(&NVIC_InitStructure);
+
+    uart_init(COM1,115200);
+    tft_init(PIN_ON_TOP, WHITE, BLACK, RED, YELLOW);
+    sonar_init();
+	
     rcc_init();
     ticks_init();
     
     leds_init();
     buttons_init();	
     uart_init(COM3,38400);
-	motor_init(MOTOR1, 11, 1200, 0, 1);
-	motor_init(MOTOR2, 11, 1200, 0, 1);
+    motor_init(MOTOR1, 11, 1200, 0, 1);
+    motor_init(MOTOR2, 11, 1200, 0, 1);
     
     uart_rx_init(COM3,&UARTOnReceiveHandler);  
     uint32_t lastticks=get_ticks();
@@ -51,17 +79,31 @@ int main() {
             if (!(lastticks%50)){
               //code here will run every 50 ms
               
+              if (robot_mode == AUTO){
+                if ((sonar_distance >= 100) && (sonar_distance <= 250)){
+                  //grab
+                }else if (sonar_distance > 1000){
+                  //Forward(300);
+                }else if (sonar_distance > 250){
+                  //Forward(300);
+                }else if (sonar_distance < 100){
+                  //Backward(300);
+                }else {
+                  Stop();
+                }
+              }
+
               if (value_received == 0){
-			    Stop();
-			  }else if (value_received <= 50){
-	  			Forward(value_received * 24);
-	  		  }else if (value_received <= 100){
-				TurnLeft((value_received - 50) * 24);
-		      }else if (value_received <= 150){
-				Backward((value_received - 100) * 24);
-			  }else if (value_received <= 200){
-				TurnRight((value_received - 150) * 24);
-			  }
+                Stop();
+              }else if (value_received <= 50){
+                Forward(value_received * 24);
+              }else if (value_received <= 100){
+                TurnLeft((value_received - 50) * 24);
+              }else if (value_received <= 150){
+                Backward((value_received - 100) * 24);
+              }else if (value_received <= 200){
+                TurnRight((value_received - 150) * 24);
+              }
               
             }
         }
