6dc7c0d8207900608b03952ae8b3b85598e3a9d1
Ethan <43922849+ethanppl@users.noreply.github.com>
2018-11-11 16:32:34 +0800

    Update main.c

--- Thrower Robot/main.c
+++ Thrower Robot/main.c
@@ -15,7 +15,7 @@
 #include "MotorControl.h"
 #include "LineSensor.h"
 
-typedef enum{AUTO, MANUAL}Operation_Mode;
+typedef enum{AUTO, MANUAL}OperationMode;
 
 u32 value_received; //value from Bluetooth connection
 void UARTOnReceiveHandler(const u8 received){
@@ -24,31 +24,8 @@ void UARTOnReceiveHandler(const u8 received){
 
 int main() {
     // Initialize Everything Here
-    char buffer[80] = {'\0'};
-
     SetSysClockTo72();
-
-    // TIMER4 Двічи за секунду викликає sonar_start(); і встановлює FLAG_ECHO = 1;
-    TIM_TimeBaseInitTypeDef TIMER_InitStructure;
-    NVIC_InitTypeDef NVIC_InitStructure;
-    RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM4, ENABLE);
-    TIM_TimeBaseStructInit(&TIMER_InitStructure);
-    TIMER_InitStructure.TIM_CounterMode = TIM_CounterMode_Up;
-    TIMER_InitStructure.TIM_Prescaler = 7200;
-    TIMER_InitStructure.TIM_Period = 5000;
-    TIM_TimeBaseInit(TIM4, &TIMER_InitStructure);
-    TIM_ITConfig(TIM4, TIM_IT_Update, ENABLE);
-    TIM_Cmd(TIM4, ENABLE);
-
-    /* NVIC Configuration */
-    /* Enable the TIM4_IRQn Interrupt */
-    NVIC_InitStructure.NVIC_IRQChannel = TIM4_IRQn;
-    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
-    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
-    NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
-    NVIC_Init(&NVIC_InitStructure);
-
-    usart_init();
+    
     tft_init(PIN_ON_TOP, WHITE, BLACK, RED, YELLOW);
     sonar_init();
 	
@@ -65,25 +42,23 @@ int main() {
     uint32_t lastticks=get_ticks();
 		int sonar_distance;
 		
-		Operation_Mode robot_mode;
+		OperationMode robot_mode;
 		
     while(1){
         if(lastticks!=get_ticks()){
             lastticks=get_ticks();
             if (!(lastticks%50)){
-              //code here will run every 50 ms
-              
-              //output the distance from the object to the ultrasonic sensor on tft in mm
-              if (FLAG_ECHO == 1){
-                tft_clear();
-                sonar_distance = sonar_get();
-                tft_prints(0,0,"%d",sonar_distance);
-                USARTSend(buffer);
-                FLAG_ECHO = 0;
-                tft_update();
-              }
+              //code here will run every 50
               
               if (robot_mode == AUTO){
+                
+                //output the distance from the object to the ultrasonic sensor on tft in mm
+                sonar_start();
+                tft_clear();
+                sonar_distance = sonar_get();
+                tft_prints(0, 0, "%d", sonar_distance);
+                tft_update();
+                
                 if ((sonar_distance >= 100) && (sonar_distance <= 250)){
                   //grab
                 }else if (sonar_distance > 1000){
