0e004addae7a89337eec8374aab1b03200920aa1
itsbinay <44058187+itsbinay@users.noreply.github.com>
2018-11-12 00:26:26 +0800

    Add files via upload

--- Thrower Robot/main.c
+++ Thrower Robot/main.c
@@ -14,6 +14,7 @@
 #include "MotorControl.h"
 //#include "LineSensor.h"
 #include "Sonar.h"
+#include <stdbool.h>
 
 typedef enum{AUTO, MANUAL}Operation_Mode;
 typedef enum{STRAIGHT_L,PUSH_FROM_LEFT}Left_Motor_Motion;
@@ -32,20 +33,27 @@ int main() {
    
     leds_init();
     buttons_init();
+		buzzer_init();
 		tft_init(PIN_ON_LEFT, BLACK, WHITE, WHITE, WHITE);
    // uart_init(COM3,115200);
-		motor_init(MOTOR1, 11, 1200, 0, 1);
-		motor_init(MOTOR2, 11, 1200, 0, 1);
+		motor_init(MOTOR1, 1, 11, 1200, 1);
+		motor_init(MOTOR2, 1, 11, 1200, 1);
     
 		//Initialising the gpio pin for the line sensor
 		gpio_init(GPIO5, GPIO_Mode_IPU);//input pin5
-		gpio_init(GPIO6, GPIO_Mode_IPU);//input pin6
+		gpio_init(GPIO6, GPIO_Mode_IPU);//input pin6 for Right line sensor
+		gpio_init(GPIO7, GPIO_Mode_IPU);
+	
 		int left_line_sensor=0;
 		int right_line_sensor=0;
 		
+		bool previous_checker,current_checker;
+		int counter=0;
+		
 		const int forwardSpeed=200;
 		const int differenceSpeed=50;
-		const int turningSpeed=50;
+		const int turningSpeed=80;
+		const int stoppingSpeed=1200;
 		
 		Operation_Mode robot_mode;
 		robot_mode=AUTO;//INITIALISE ROBOT MODE AS AUTO
@@ -58,52 +66,84 @@ int main() {
 			
 			left_line_sensor=gpio_read(GPIO5);
 			right_line_sensor=gpio_read(GPIO6);
-			tft_clear();
-			tft_prints(0,0,"Left Line Sensor:");
-			tft_prints(0,1,"%d",left_line_sensor);
-			tft_prints(0,2,"Right Line Sensor:");
-			tft_prints(0,3,"%d",right_line_sensor);
-			tft_prints(0,4,"Left Motor Speed:");
-			tft_prints(0,6,"Right Motor Speed:");
 			
+			tft_clear();
+			tft_prints(0,0,"Left Sensor: %d",left_line_sensor);
+			tft_prints(0,1,"Right Line Sensor: %d",right_line_sensor);
+			tft_prints(0,2,"Left Motor Speed:");
+			tft_prints(0,4,"Right Motor Speed:");
+			tft_prints(0,6,"Counter Value:");
+			tft_update();
 			
 			static u32 last_ticks=0;
 			if((this_ticks-last_ticks)>=200){//Code from here and below will only work once every 200 ms
 				
-				if(left_line_sensor==0 && right_line_sensor==0){
-					Forward(forwardSpeed);
-					
-					tft_prints(0,5,"%d",forwardSpeed);
-					tft_prints(0,7,"%d",forwardSpeed);
-					tft_update();
+				
+				/*
+				 *The following code is to use the line sensor for counting grid function
+				 *
+				*/
+				if(gpio_read(GPIO5)){current_checker=true;}
+				else{current_checker=false;}	
+//-------------------------------------------------------------------------------				
+				if(current_checker!=previous_checker){
+					counter++;
+					tft_prints(0,7,"%d",counter);
 				}
-				else if(left_line_sensor==1 && right_line_sensor==0){
-					LForward(forwardSpeed);//Left motor remains constant
-					RForward(forwardSpeed-differenceSpeed);//Right motor slows down
+//Everytime there is a change in the input value compared to the value in the previous tick, counter increases
+//-------------------------------------------------------------------------------
+				if(gpio_read(GPIO5)){previous_checker=true;}
+				else{previous_checker=false;}
+				
+				
+				tft_prints(0,1,"%d",counter);
+				tft_update();
+				
+				
+				/*
+				 *The code from here and below is for the side sensors to bounce the thrower robot away from the lines
+				 *
+				 */
+				
+				//if(robot_mode==AUTO){
+					buzzer_on();
+					if(left_line_sensor==0 && right_line_sensor==0){//If side sensor doesn't sense the line
+						Forward(forwardSpeed);
 					
-					tft_prints(0,5,"%d",forwardSpeed);
-					tft_prints(0,7,"%d",forwardSpeed-differenceSpeed);
-					tft_update();
-				}
-				else if(left_line_sensor==0 && right_line_sensor==1){
-					LForward(forwardSpeed-differenceSpeed);
-					RForward(forwardSpeed);
+						tft_prints(0,3,"%d",forwardSpeed);
+						tft_prints(0,5,"%d",forwardSpeed);
+						tft_update();
+					}
+					else if(left_line_sensor==1 && right_line_sensor==0){
+						LForward(forwardSpeed);//Left motor remains constant
+						RForward(forwardSpeed-differenceSpeed);//Right motor slows down
 					
-					tft_prints(0,5,"%d",forwardSpeed-differenceSpeed);
-					tft_prints(0,7,"%d",forwardSpeed);
-					tft_update();
-				}
-				else if(left_line_sensor==1 && right_line_sensor==1){
-					Stop();
+						tft_prints(0,3,"%d",forwardSpeed);
+						tft_prints(0,5,"%d",forwardSpeed-differenceSpeed);
+						tft_update();
+					}
+					else if(left_line_sensor==0 && right_line_sensor==1){
+						LForward(forwardSpeed-differenceSpeed);
+						RForward(forwardSpeed);
 					
-					tft_prints(0,5,"0");
-					tft_prints(0,7,"0");
-					tft_update();
-				}
+						tft_prints(0,3,"%d",forwardSpeed-differenceSpeed);
+						tft_prints(0,5,"%d",forwardSpeed);
+						tft_update();
+					}
+					else if(left_line_sensor==1 && right_line_sensor==1){
+						Stop();
+					
+						tft_prints(0,3,"7200");
+						tft_prints(0,5,"7200");
+						tft_update();
+					}
+				
+				
 			}
-			//UARTOnReceiveHandler();
+			
+			
 //----------------------------------------------------------------------------------------------------------------------						
-			/*				else if(robot_mode==MANUAL){
+							if(robot_mode==MANUAL){
 								if (value_received == 0){
 									Stop();
 								}else if (value_received <= 50){
@@ -115,8 +155,7 @@ int main() {
 								}else if (value_received <= 200){
 									TurnRight((value_received - 150) * 24);
 								}
-							}*/
-       // }
+							}
 			
 			}
 		}
