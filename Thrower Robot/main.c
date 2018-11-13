7e8f79ff9ead3cd05b9caa6e192c5a040b2d82cf
itsbinay <44058187+itsbinay@users.noreply.github.com>
2018-11-13 14:24:25 +0800

    Counting System of grids passed complete

--- Thrower Robot/main.c
+++ Thrower Robot/main.c
@@ -21,9 +21,12 @@ typedef enum{STRAIGHT_L,PUSH_FROM_LEFT}Left_Motor_Motion;
 typedef enum{STRAIGHT_R,PUSH_FROM_RIGHT}Right_Motor_Motion;
 
 u32 value_received; //value from Bluetooth connection
+
 void UARTOnReceiveHandler(const u8 received){
   value_received = received;
 }
+
+
 
 int main() {
     // Initialize Everything Here
@@ -33,16 +36,18 @@ int main() {
    
     leds_init();
     buttons_init();
-		buzzer_init();
+	//	sonar_init();//Comment this line if not using sonar
+	
 		tft_init(PIN_ON_LEFT, BLACK, WHITE, WHITE, WHITE);
-   // uart_init(COM3,115200);
+		uart_init(COM3,38400);//Definition of UART PORT 3
+	
 		motor_init(MOTOR1, 1, 11, 1200, 1);
 		motor_init(MOTOR2, 1, 11, 1200, 1);
     
 		//Initialising the gpio pin for the line sensor
-		gpio_init(GPIO5, GPIO_Mode_IPU);//input pin5
+		gpio_init(GPIO5, GPIO_Mode_IPU);//input pin5 for the Left line Sensor
 		gpio_init(GPIO6, GPIO_Mode_IPU);//input pin6 for Right line sensor
-		gpio_init(GPIO7, GPIO_Mode_IPU);
+		gpio_init(GPIO7, GPIO_Mode_IPU);//input pin 7 for the counter
 	
 		int left_line_sensor=0;
 		int right_line_sensor=0;
@@ -66,6 +71,7 @@ int main() {
 			
 			left_line_sensor=gpio_read(GPIO5);
 			right_line_sensor=gpio_read(GPIO6);
+	
 			
 			tft_clear();
 			tft_prints(0,0,"Left Sensor: %d",left_line_sensor);
@@ -73,40 +79,40 @@ int main() {
 			tft_prints(0,2,"Left Motor Speed:");
 			tft_prints(0,4,"Right Motor Speed:");
 			tft_prints(0,6,"Counter Value:");
+			tft_prints(0,7,"%d",counter);
 			tft_update();
 			
 			static u32 last_ticks=0;
-			if((this_ticks-last_ticks)>=200){//Code from here and below will only work once every 200 ms
+			if((this_ticks-last_ticks)>=500){//Code from here and below will only work once every 200 ms
 				
 				
 				/*
 				 *The following code is to use the line sensor for counting grid function
 				 *
 				*/
-				if(gpio_read(GPIO5)){current_checker=true;}
+				if(gpio_read(GPIO7)){current_checker=true;}
 				else{current_checker=false;}	
 //-------------------------------------------------------------------------------				
-				if(current_checker!=previous_checker){
+				if(current_checker!=previous_checker && previous_checker==true){
 					counter++;
-					tft_prints(0,7,"%d",counter);
 				}
 //Everytime there is a change in the input value compared to the value in the previous tick, counter increases
 //-------------------------------------------------------------------------------
-				if(gpio_read(GPIO5)){previous_checker=true;}
+				if(gpio_read(GPIO7)){previous_checker=true;}
 				else{previous_checker=false;}
 				
 				
-				tft_prints(0,1,"%d",counter);
+				tft_prints(0,7,"%d",counter);
 				tft_update();
 				
-				
+//-------------------------------------------------------------------------------------	
 				/*
 				 *The code from here and below is for the side sensors to bounce the thrower robot away from the lines
 				 *
 				 */
 				
 				//if(robot_mode==AUTO){
-					buzzer_on();
+					
 					if(left_line_sensor==0 && right_line_sensor==0){//If side sensor doesn't sense the line
 						Forward(forwardSpeed);
 					
@@ -138,24 +144,23 @@ int main() {
 						tft_update();
 					}
 				
-				
-			}
+				}
 			
 			
 //----------------------------------------------------------------------------------------------------------------------						
-							if(robot_mode==MANUAL){
-								if (value_received == 0){
-									Stop();
-								}else if (value_received <= 50){
-									Forward(value_received * 24);
-								}else if (value_received <= 100){
-									TurnLeft((value_received - 50) * 24);
-								}else if (value_received <= 150){
-									Backward((value_received - 100) * 24);
-								}else if (value_received <= 200){
-									TurnRight((value_received - 150) * 24);
-								}
-							}
-			
+					if(robot_mode==MANUAL){
+						if (value_received == 0){
+							Stop();
+						}else if (value_received <= 50){
+							Forward(value_received * 24);
+						}else if (value_received <= 100){
+							TurnLeft((value_received - 50) * 24);
+						}else if (value_received <= 150){
+							Backward((value_received - 100) * 24);
+						}else if (value_received <= 200){
+							TurnRight((value_received - 150) * 24);
+						}
+					}
 			}
-		}
+
+	}
