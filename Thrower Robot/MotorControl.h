d0abcfd06e022f391f314f7b4540de1371aeacb2
itsbinay <44058187+itsbinay@users.noreply.github.com>
2018-11-26 10:40:05 +0800

    Update MotorControl.h

--- Thrower Robot/MotorControl.h
+++ Thrower Robot/MotorControl.h
@@ -1,5 +1,8 @@
 #include "pwm.h"
 
+#define LMotor MOTOR1
+#define RMotor MOTOR3
+
 void LForward(u16 a);
 void RForward(u16 a);
 void LBackward(u16 a);
@@ -10,3 +13,9 @@ void Backward (u16 speed);
 void TurnLeft (u16 speed);
 void TurnRight (u16 speed);
 void Stop();
+
+void MForward (u16 speed);
+void MBackward (u16 speed);
+void MTurnLeft (u16 speed);
+void MTurnRight (u16 speed);
+void MStop();
